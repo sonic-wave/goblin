bb826dbb8aa3ac1a7a05102b4e8c1c4492443058
Ivan Khokhlov <mozhno@MacBook-Air-Mozhno.local>
2023-12-13 15:00:03 +0300

    init commit

---
+++ src/js/app.js
@@ -0,0 +1,20 @@
+const goblin = document.querySelectorAll('.cell');
+const goblinImage = document.createElement('img');
+
+goblinImage.src = '/src/goblin.png'
+
+let numArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+let randomNum = 0;
+let temp = 0;
+
+const newInterval = setInterval(() => {
+    if(!numArray.length) {
+        numArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+    }
+    randomNum = Math.floor(Math.random() * numArray.length);    
+    temp = numArray[randomNum];
+    numArray = numArray.filter((element) => element !== temp);
+    console.log(`temp: ${temp}`);
+    console.log(`numArray: ${randomNum}`)
+    goblin[temp].appendChild(goblinImage);
+}, 1000)
