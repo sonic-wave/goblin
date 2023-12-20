e25fea6a04477ddf2fe46d7a6fe4f23612d7448f
Ivan Khokhlov <mozhno@MacBook-Air-Mozhno.local>
2023-12-20 15:21:13 +0300

    lint check

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,4 @@
-const goblin = document.querySelectorAll('.cell');
+const goblin = document.querySelectorAll(".cell");
 // const goblinImage = document.createElement('img');
 
 // goblinImage.src = '../img/goblin.png'
@@ -8,16 +8,16 @@ let randomNum = 0;
 let temp = 0;
 
 const newInterval = setInterval(() => {
-    if(!numArray.length) {
-        numArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+  if (!numArray.length) {
+    numArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+  }
+  goblin.forEach((item) => {
+    if (item.classList.contains("field-item-img")) {
+      item.classList.remove("field-item-img");
     }
-    goblin.forEach((item) => {
-        if (item.classList.contains('field-item-img')) {
-            item.classList.remove('field-item-img');
-        };
-})
-    randomNum = Math.floor(Math.random() * numArray.length);    
-    temp = numArray[randomNum];
-    numArray = numArray.filter((element) => element !== temp);
-    goblin[temp].classList.add('field-item-img');
-}, 1000)
+  });
+  randomNum = Math.floor(Math.random() * numArray.length);
+  temp = numArray[randomNum];
+  numArray = numArray.filter((element) => element !== temp);
+  goblin[temp].classList.add("field-item-img");
+}, 1000);
